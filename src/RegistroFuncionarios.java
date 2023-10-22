79e1b3955000a74ef8a8037015974b1714e36a48
GabrielAldiney <102868870+GabrielAldiney@users.noreply.github.com>
2023-10-22 13:50:57 -0300

    Update RegistroFuncionarios.java

--- src/RegistroFuncionarios.java
+++ src/RegistroFuncionarios.java
@@ -1,2 +1,29 @@
+import java.util.List;
+import java.util.ArrayList;
+
+
 public class RegistroFuncionarios {
+    private static RegistroFuncionarios instancia = null;
+
+    private List<Funcionario> funcionarios = new ArrayList<>();
+
+    private RegistroFuncionarios() {
+    }
+
+    public static RegistroFuncionarios getInstancia() {
+        if (instancia == null) {
+            instancia = new RegistroFuncionarios();
+        }
+        return instancia;
+    }
+
+    public void adicionarFuncionario(Funcionario funcionario) {
+        funcionarios.add(funcionario);
+    }
+
+    public void listarFuncionarios() {
+        for (Funcionario funcionario : funcionarios) {
+            System.out.println("Nome: " + funcionario.getNome() + ", Cargo: " + funcionario.getCargo());
+        }
+    }
 }
