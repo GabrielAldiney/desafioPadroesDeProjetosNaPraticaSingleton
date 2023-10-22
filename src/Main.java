f5ec50d49a3798c19206e207cd170952ffc1e019
GabrielAldiney <102868870+GabrielAldiney@users.noreply.github.com>
2023-10-22 13:50:21 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        RegistroFuncionarios registro = RegistroFuncionarios.getInstancia();
+
+        registro.adicionarFuncionario(new Funcionario("Gabriel Aldiney", "Desenvolvedor Java"));
+        registro.adicionarFuncionario(new Funcionario("João", "Gerente de projetos"));
+        registro.adicionarFuncionario(new Funcionario("Mateus", "Designer"));
+        registro.adicionarFuncionario(new Funcionario("Isabela", "Analista de Qualidade"));
+        registro.adicionarFuncionario(new Funcionario("Eduardo", "Suporte Técnico"));
+
+
+        System.out.println("Funcionários na empresa:");
+        registro.listarFuncionarios();
     }
 }
